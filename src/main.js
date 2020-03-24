974deb404747950eafbcfd9c95425922367f291c
mhafet17 <do.crazy192@gmail.com>
2020-03-25 03:14:15 +0700

    import bootstrap css & js

--- src/main.js
+++ src/main.js
@@ -1,39 +1,46 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 //import Vue Router
-import VueRouter from 'vue-router'
+import VueRouter from "vue-router";
 
 //menggunkan Vue Router di Vue JS
 Vue.use(VueRouter);
 
-//import components
-import PostsIndex from './components/posts/Index'
-import PostsCreate from './components/posts/Create'
-import PostsEdit from './components/posts/Edit'
+//import bootstrap css & js
+import "bootstrap/dist/css/bootstrap.css";
+import "jquery/dist/jquery.min";
+import "popper.js/dist/popper.min";
+import "bootstrap/dist/js/bootstrap.min";
 
-Vue.config.productionTip = false
+//import components
+import PostsIndex from "./components/posts/Index";
+import PostsCreate from "./components/posts/Create";
+import PostsEdit from "./components/posts/Edit";
+
+Vue.config.productionTip = false;
 
 const router = new VueRouter({
-  routes: [{
-      path: '/',
-      name: 'posts',
+  routes: [
+    {
+      path: "/",
+      name: "posts",
       component: PostsIndex
     },
     {
-      path: '/create',
-      name: 'create',
+      path: "/create",
+      name: "create",
       component: PostsCreate
     },
     {
-      path: '/edit/:id',
-      name: 'edit',
+      path: "/edit/:id",
+      name: "edit",
       component: PostsEdit
     }
   ],
-  mode: 'history'
-})
+  mode: "history"
+});
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
