e7346dd3edbee53722601bf3c2afda84f74a3fdb
mhafet17 <do.crazy192@gmail.com>
2020-06-08 20:39:59 +0700

    create views Home

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col-md-4">
+          <Card />
+        </div>
+        <div class="col-md-4">
+          <Card />
+        </div>
+        <div class="col-md-4">
+          <Card />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "@/Latihan/Card.vue";
+
+export default {
+  components: {
+    Card,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
