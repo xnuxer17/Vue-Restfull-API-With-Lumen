9937758f36125b978034b05d053bc3dcb17d0df9
mhafet17 <do.crazy192@gmail.com>
2020-06-08 20:38:22 +0700

    create template

---
+++ src/components/Template.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="mt-3">
+    <h1>Belajar Penulisan Template</h1>
+
+    <!-- Data Raw HTML -->
+    <hr />
+    <br />
+    <h3>Raw HTML</h3>
+    <span v-html="danger"></span>
+    <span v-html="primary"></span>
+    <hr />
+
+    <!-- Data Attribute -->
+    <hr />
+    <br />
+    <h3>Data Attribute</h3>
+    <h1 v-bind:class="title">{{ message }}</h1>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // Raw HTML
+      danger: "<span class='btn btn-danger btn-block'> Danger </span>",
+      primary: "<span class='btn btn-primary btn-block'>Primary </span>",
+
+      // Data Attribute
+      message: "VueJS Framework",
+      title: "title",
+
+      //   JS Expression
+    };
+  },
+};
+</script>
+
+<style>
+.title {
+  color: lime;
+}
+</style>
