575b55a6f6073f88737847a88cc267ab826ebb46
mhafet17 <do.crazy192@gmail.com>
2020-06-08 20:37:43 +0700

    create index

---
+++ src/components/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="text-center mt-5">
+    <h1>Belajar Vue</h1>
+    <h1>
+      <router-link to="/lifecycle">1.lifecycle</router-link>
+      <br />
+      <router-link to="/template">2.template</router-link>
+      <br />
+      <router-link to="/properti">3.properti</router-link>
+      <br />
+      <router-link to="/directive">4.directive</router-link>
+      <br />
+      <router-link to="/form">5.form</router-link>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
