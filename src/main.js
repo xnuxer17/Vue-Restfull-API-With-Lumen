7f24232a37599fef2cc3461fab1d897f40b0d354
mhafet17 <do.crazy192@gmail.com>
2020-06-08 20:41:33 +0700

    create new routing

--- src/main.js
+++ src/main.js
@@ -13,9 +13,16 @@ import "popper.js/dist/popper.min";
 import "bootstrap/dist/js/bootstrap.min";
 
 //import components
-import PostsIndex from "./components/posts/Index";
-import PostsCreate from "./components/posts/Create";
-import PostsEdit from "./components/posts/Edit";
+import Lifecycle from "./components/LifeCycle";
+import Template from "./components/Template";
+import Properti from "./components/Properti";
+import Directive from "./components/Directive";
+import Index from "./components/Index";
+import Form from "./components/Form";
+import Filtering from "./Latihan/Filtering";
+import Handle from "./Latihan/HandleSubmit";
+import Home from "./views/Home";
+import User from "./views/User";
 
 Vue.config.productionTip = false;
 
@@ -23,24 +30,59 @@ const router = new VueRouter({
   routes: [
     {
       path: "/",
-      name: "posts",
-      component: PostsIndex
+      name: "index",
+      component: Index,
     },
     {
-      path: "/create",
-      name: "create",
-      component: PostsCreate
+      path: "/home",
+      name: "home",
+      component: Home,
     },
     {
-      path: "/edit/:id",
-      name: "edit",
-      component: PostsEdit
-    }
+      path: "/user/:id",
+      name: "User",
+      component: User,
+    },
+    {
+      path: "/lifecycle",
+      name: "lifecycle",
+      component: Lifecycle,
+    },
+    {
+      path: "/template",
+      name: "template",
+      component: Template,
+    },
+    {
+      path: "/properti",
+      name: "properti",
+      component: Properti,
+    },
+    {
+      path: "/directive",
+      name: "directive",
+      component: Directive,
+    },
+    {
+      path: "/form",
+      name: "form",
+      component: Form,
+    },
+    {
+      path: "/filtering",
+      name: "filtering",
+      component: Filtering,
+    },
+    {
+      path: "/handle",
+      name: "handle",
+      component: Handle,
+    },
   ],
-  mode: "history"
+  mode: "history",
 });
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
