0fb4681c91dff077043e1489fc18531dde23442f
mhafet17 <do.crazy192@gmail.com>
2020-06-08 20:39:08 +0700

    create Latihan Card

---
+++ src/Latihan/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <img class="card-img-top" src="../images/gamamaru.png" alt="" />
+    <div class="card-body">
+      <h4 class="card-title">{{ title }}</h4>
+      <p class="card-text">{{ desc }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "gamamaru",
+      desc: "gamamaru the sannin legendary",
+    };
+  },
+};
+</script>
+
+<style></style>
