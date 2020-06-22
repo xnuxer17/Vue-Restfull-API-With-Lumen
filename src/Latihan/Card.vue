056322169b110e00b9b15aa3f9ea5a40cfa0edc4
mhafet17 <do.crazy192@gmail.com>
2020-06-23 05:55:09 +0700

    modifikasi Card

--- src/Latihan/Card.vue
+++ src/Latihan/Card.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="card">
-    <img class="card-img-top" src="../images/gamamaru.png" alt="" />
+    <img class="card-img-top" src="../images/gamamaru.png" alt />
     <div class="card-body">
       <h4 class="card-title">{{ title }}</h4>
       <p class="card-text">{{ desc }}</p>
+      <p class="card-text">hello {{ name }}</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'Card',
+  props: ["name"],
   data() {
     return {
       title: "gamamaru",
